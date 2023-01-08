78309298d1e9c6f2e135d69703bf3e07e8e46a79
616164124@qq.com <616164124@qq.com>
2023-01-09 05:26:54 +0800

    2023年1月9日

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 // import Index from "./day09/index"
 // import store from "./day09/store"
-import Index from "./day10/index";
-import Day10 from "./Day10.js";
-import Day11 from "./Day11.js";
-import Day12 from "./Day12";
-import Day13 from "./Day13";
-import Day15 from "./Day15";
-import Day12_1 from "./day12/day12"
+// import Index from "./day10/index";
+// import Day10 from "./Day10.js";
+// import Day11 from "./Day11.js";
+// import Day12 from "./Day12";
+// import Day13 from "./Day13";
+// import Day15 from "./Day15";
+// import Day12_1 from "./day12/day12"
+import HelloWorld from "./index04";
 class Parent extends Component {
   render() {
     const style1 = {
@@ -21,26 +22,29 @@ class Parent extends Component {
       <div>
         {/*<Provider store={store}>*/}
         <div>
-          <Index />
+
+          <HelloWorld />
+
+          {/*<Index />*/}
           <div style={style1} />
-          <Day10 />
+          {/*<Day10 />*/}
           <div style={style1} />
-          <Day11 />
+          {/*<Day11 />*/}
         </div>
         {/*</Provider>*/}
         <div style={style1}></div>
-        <h1>day12</h1>
-        <Day12 />
+        {/*<h1>day12</h1>*/}
+        {/*<Day12 />*/}
         <div style={style1}></div>
-        <Day13 />
+        {/*<Day13 />*/}
         <div style={style1}></div>
         <h1>day15</h1>
-        <Day15 />
+        {/*<Day15 />*/}
         <div style={style1}></div>
-        <h1>day16</h1>
+        {/*<h1>day16</h1>*/}
         <div style={style1}></div>
-        <h1>Day12_1</h1>
-        <Day12_1/>
+        {/*<h1>Day12_1</h1>*/}
+        {/*<Day12_1/>*/}
       </div>
     );
   }
